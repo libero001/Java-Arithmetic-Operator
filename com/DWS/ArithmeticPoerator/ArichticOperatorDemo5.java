111def2710ca5140678e3b523382fa28a2397c4a
libero001 <18330620953@163.com>
2025-11-22 18:01:05 +0800

    自增自减运算符

---
+++ com/DWS/ArithmeticPoerator/ArichticOperatorDemo5.java
@@ -0,0 +1,17 @@
+package com.DWS.ArithmeticPoerator;
+
+public class ArichticOperatorDemo5 {
+    public static void main(String[] args) {
+        //++和--
+        int a=10;
+        a++;
+        System.out.println(a);//11
+        ++a;
+        System.out.println(a);//12
+        a--;
+        System.out.println(a);//11
+        --a;
+        System.out.println(a);//10
+
+    }
+}
